4673a47d14ec15cd34c1a5916469235a02cec2fd
diffitask <ginger.ll@mail.ru>
2022-02-03 04:40:29 +0300

    updated dfs_graph

--- hw01/ast_tree.py
+++ hw01/ast_tree.py
@@ -1,33 +1,28 @@
 import ast
-import astunparse
+import networkx as nx
+import matplotlib.pyplot as plt
+
 
 class AstGraph:
-    num = 0
-    labels_list = {}
+    def __init__(self):
+        self.G = nx.Graph()
+        self.node_cnt = 0
+        self.labels_list = {}
 
-    def dfs_graph(self, G, ast_object):
-        if isinstance(ast_object, list):
-            G.add_node(self.num)
-            self.labels_list[self.num] = 'list'
+    def dfs_graph(self, node):
+        self.G.add_node(self.node_cnt)
+        self.labels_list[self.node_cnt] = ast.unparse(node)
+        cur_num = self.node_cnt
+        self.node_cnt += 1
 
-            cur_num = self.num
-            self.num += 1
-            for i in ast_object:
-                G.add_edge(cur_num, self.dfs_graph(G, i))
-                self.num += 1
-            return cur_num
+        for ch in ast.iter_child_nodes(node):
+            self.G.add_edge(cur_num, self.dfs_graph(ch))
+        return cur_num
 
-        elif isinstance(ast_object, ast.AST):
-            self.labels_list[self.num] = ast.unparse(ast_object)
-            cur_num = self.num
-            self.num += 1
-            for elem in ast_object._fields:
-                attr = ast_object.__getattribute__(elem)
-                if attr is not None:
-                    G.add_edge(cur_num, self.dfs_graph(G, attr))
-            return cur_num
-        else:
-            self.labels_list[self.num] = str(ast_object)
-            cur_num = self.num
-            self.num += 1
-            return cur_num
+    def draw_graph(self):
+        plt.figure(0, (10, 10))
+        nx.draw(self.G,
+                nx.drawing.nx_agraph.graphviz_layout(self.G, prog="dot"),
+                with_labels=True,
+                labels=self.labels_list)
+        plt.show()
