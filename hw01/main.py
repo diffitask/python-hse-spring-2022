0bfa8728f7cab3c8adeff887b281602e95d36a17
diffitask <ginger.ll@mail.ru>
2022-02-03 04:40:29 +0300

    updated dfs_graph

--- hw01/main.py
+++ hw01/main.py
@@ -1,21 +1,17 @@
-import networkx as nx
 import ast
-import matplotlib.pyplot as plt
-import ast_tree
+from ast_tree import AstGraph
 
-f = open('fibonacci_func.py', 'r')
-ast_object = ast.parse(f.read())
 
-#print(ast.dump(ast_object))
-#print(ast_object._fields)
-# print(ast.unparse(ast_object))
+def main():
+    # create ast from file
+    f = open('fibonacci_func.py', 'r')
+    ast_object = ast.parse(f.read())
 
-G = nx.Graph()
-graph = ast_tree.AstGraph()
-graph.dfs_graph(G, ast_object)
-plt.figure(0, (10, 10))
-nx.draw(G,
-        nx.drawing.nx_agraph.graphviz_layout(G, prog="dot"),
-        with_labels=True,
-        labels=graph.labels_list)
-plt.show()
+    # create graph
+    ast_graph = AstGraph()
+    ast_graph.dfs_graph(ast_object)
+    ast_graph.draw_graph()
+
+
+if __name__ == '__main__':
+    main()
