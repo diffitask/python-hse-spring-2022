0398a7bfcda12fe069e16720c02beab0be4d9cda
diffitask <ginger.ll@mail.ru>
2022-02-16 01:44:50 +0300

    added setup.py

---
+++ hw01/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="ast-graph-fibonacci-generator",
+    version="0.0.1",
+    author="Evgenia Fedotova",
+    description="Generator of ast_graph of fibonacci func",
+    url="https://github.com/diffitask/python-hse-spring-2022",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+)
